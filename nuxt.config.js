fe8515c56e87dd9f5e1acc041fb9500f0cd0e13a
kaiya-m <marie.kaiya@persol.co.jp>
2022-11-07 12:02:11 +0900

    :art: test

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/kaiya-m.github.io/'
+    base: '/nuxt-github-sample/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
